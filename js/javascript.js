f21b3ae13e89b9de65212d80aaa7aba7842f9c0c
aniketkote <aniketkolte1120@gmail.com>
2021-07-08 21:11:48 +0530

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -104,3 +104,18 @@
             }
         });
       });
+
+      // smooth scroll to anchor, with option of hash appearing in url. Thanks:
+// https://paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        // window.location.hash = target;
+	    });
+	});
+});
