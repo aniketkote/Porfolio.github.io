39400ea175f5403d8fc242c97a9add1122d141b8
aniketkote <aniketkolte1120@gmail.com>
2021-07-12 19:57:25 +0530

    x
    x

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Aniket", "Web Developer", "Java Developer", "Blogger"];
+const textArray = ["Aniket", "Web Developer", "Java Developer"];
 const typingDelay = 80;
 const erasingDelay = 80;
 const newTextDelay = 2000; // Delay between current and next text
